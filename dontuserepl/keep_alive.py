74e98b7f0e813f4d38477856a062d552f0c0970d
chrisdewa <62080903+chrisdewa@users.noreply.github.com>
2021-09-07 21:24:40 -0500

    webserver runs on same loop as the bot instead of a different thread

--- dontuserepl/keep_alive.py
+++ dontuserepl/keep_alive.py
@@ -10,20 +10,15 @@ __all__ = (
 def web_server():
     def main(request):
         return web.Response(text="I'm alive")
+        
     app = web.Application()
     app.add_routes([web.get('/', main)])
     runner = web.AppRunner(app)
     return runner
 
-def run_server(runner):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+def keep_alive():
+    runner = web_server()
+    loop = asyncio.get_event_loop()
     loop.run_until_complete(runner.setup())
     site = web.TCPSite(runner, '0.0.0.0', 8080)
     loop.run_until_complete(site.start())
-    loop.run_forever()
-
-def keep_alive():
-    runner = web_server()
-    server = Thread(target=run_server, args=(runner,))
-    server.start()
